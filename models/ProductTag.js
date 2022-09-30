27f5fb7b9ccb4419c98d8a04d503210b0de5963c
Dustin Davisson <dustin.davisson@gmail.com>
2022-09-30 16:47:51 -0700

    Model definition.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,31 @@
 const { Model, DataTypes } = require('sequelize');
-
+const { Product, ProductTag } = require('./index');
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: ProductTag,
+        key: id,
+      },
+    },
   },
   {
     sequelize,
