217cc90e2f9da0a98bb81ffd6c6ac66dcc14d8ab
Dustin Davisson <dustin.davisson@gmail.com>
2022-10-03 12:08:46 -0700

    Constraints.

--- models/index.js
+++ models/index.js
@@ -5,13 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onUpdate: 'CASCADE',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(ProductTag, {
+  foreignKey: 'product_id',
+  constraints: false,
+});
 // Tags belongToMany Products (through ProductTag)
-
+ProductTag.hasMany(Tag, {
+  foreignKey: 'tag_id',
+  constraints: false,
+});
 module.exports = {
   Product,
   Category,
